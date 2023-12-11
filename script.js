a85a68487fc2954ccf878bc81abe1f024ce95a22
acneyouth1996 <yh849@cornell.edu>
2023-12-11 00:58:31 -0800

    add audio

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const lyrics = [
-        "How do I make you like me",
-        "How do I make you have the same feeling I do",
-        "Like I want to know everything about you",
-        "and be around you all the time",
-        "How do I tell you I want that",
-        "and not scare you away already",
-        "Because I know we are nearly strangers",
-        "I don't know you",
-        "but GOD, I want to",
-        "How do I make you like me",
-        "like I know I could to you."
+  const lyrics = [
+      "How do I make you like me",
+      "How do I make you have the same feeling I do",
+      "Like I want to know everything about you",
+      "and be around you all the time",
+      "How do I tell you I want that",
+      "and not scare you away already",
+      "Because I know we are nearly strangers",
+      "I don't know you",
+      "but GOD, I want to",
+      "How do I make you like me",
+      "like I know I could to you."
+  ];
 
-    ];
+  const delay = 40;
+  const lyricsElement = document.getElementById("lyrics");
+  const startButton = document.getElementById("startButton");
+  const audio = new Audio('sound.mp3'); // replace with the path to your audio file
 
-    const delay = 40; 
-    const lyricsElement = document.getElementById("lyrics");
+  function displayLyrics() {
+      return new Promise(async (resolve) => {
+          audio.play();
+          for (const line of lyrics) {
+              for (const char of line) {
+                  lyricsElement.textContent += char;
+                  await new Promise((charResolve) => setTimeout(charResolve, delay));
+              }
 
+              lyricsElement.innerHTML += "<br>";
 
-    async function displayLyrics() {
-        for (const line of lyrics) {
-            for (const char of line) {
-                lyricsElement.textContent += char;
-                await new Promise((resolve) => setTimeout(resolve, delay));
-            }
+              await new Promise((lineResolve) => setTimeout(lineResolve, delay * 10));
 
-            lyricsElement.innerHTML += "<br>";
+              lyricsElement.innerHTML = "";
+              await new Promise((emptyResolve) => setTimeout(emptyResolve, delay * 10));
+          }
+          resolve();
+      });
+  }
 
-            await new Promise((resolve) => setTimeout(resolve, delay * 10));
-
-            lyricsElement.innerHTML = "";
-
-            await new Promise((resolve) => setTimeout(resolve, delay * 10));
-        }
-    }
-
-    displayLyrics();
+  startButton.addEventListener("click", async () => {
+    startButton.style.display = "none"; // Hide the button after clicking
+    await displayLyrics();
+  });
 });
-
-
-
-
